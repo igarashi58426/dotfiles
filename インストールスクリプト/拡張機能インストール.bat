f71d54fe3d0373ed4ac7e6228e160584aa9fd917
igarashi58426 <igarasi0719@gmail.com>
2023-04-23 04:19:27 +0900

    vscode拡張機能のインストールを自動化
    - 拡張機能をインストールするコマンドを
    羅列したbatスクリプトを作成
    - setting.jsonの拡張機能一覧を削除
    - 実行結果のResultファイルを
    gitignoreに追加

---
+++ インストールスクリプト/拡張機能インストール.bat
@@ -0,0 +1,114 @@
+
+
+(
+@REM ====================アイコン====================
+code --install-extension PKief.material-icon-theme
+code --install-extension vscode-icons-team.vscode-icons
+
+@REM ====================日本語====================
+@REM 日本語パック
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+@REM 日本語単語区切り
+code --install-extension sgryjp.japanese-word-handler
+
+
+@REM ====================便利ユーティリティ====================
+@REM ファイル名コピー
+code --install-extension nemesv.copy-file-name
+@REM ステータスバーに時計表示
+code --install-extension Compulim.vscode-clock
+@REM ステータスバーに文字数カウント
+code --install-extension 8amjp.charactercount
+@REM base64変換
+code --install-extension adamhartford.vscode-base64
+@REM 連番数字挿入
+code --install-extension Asuka.insertnumbers
+@REM CSVカラムハイライト
+code --install-extension mechatroner.rainbow-csv
+@REM 複数連続ショートカット作成
+code --install-extension ryuta46.multi-command
+
+
+@REM ====================追加エディタ====================
+
+@REM PDFの閲覧
+code --install-extension tomoki1207.pdf
+@REM CSVエディタ
+code --install-extension janisdd.vscode-edit-csv
+@REM HEXエディタ
+code --install-extension ms-vscode.hexeditor
+@REM シリアルモニター
+code --install-extension ms-vscode.vscode-serial-monitor
+
+
+@REM ====================コーディング支援====================
+@REM path補完
+code --install-extension ionutvmi.path-autocomplete
+@REM スペルチェック
+code --install-extension streetsidesoftware.code-spell-checker
+
+@REM ====================マークダウン====================
+@REM マークダウンユーティリティパック
+code --install-extension yzhang.markdown-all-in-one
+@REM マークダウンPDF出力
+code --install-extension yzane.markdown-pdf
+@REM プレビューマークダウンgithub表示
+code --install-extension bierner.markdown-preview-github-styles
+@REM エクセルからコピペ表作成
+code --install-extension csholmq.excel-to-markdown-table
+
+@REM ====================plantUML====================
+code --install-extension jebbs.plantuml
+
+@REM ====================git====================
+@REM Git履歴のグラフ表示
+code --install-extension mhutchie.git-graph
+
+@REM ====================Docker====================
+@REM Docker制御
+code --install-extension ms-azuretools.vscode-docker
+@REM Docker Compose制御
+code --install-extension p1c2u.docker-compose
+
+
+@REM ====================AHK====================
+code --install-extension cweijan.vscode-autohotkey-plus
+
+@REM ====================HTML====================
+@REM CSSサポート
+code --install-extension ecmel.vscode-html-css
+
+@REM ====================python開発====================
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+@REM jupyter関係
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+
+@REM ====================C/C++言語====================
+code --install-extension ms-vscode.cpptools
+
+@REM ====================リモート開発====================
+@REM リモート開発
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+@REM リモートSSH
+code --install-extension ms-vscode-remote.remote-ssh
+@REM リモートSSH設定
+code --install-extension ms-vscode-remote.remote-ssh-edit
+@REM WSLリモート開発
+code --install-extension ms-vscode-remote.remote-wsl
+@REM コンテナリモート開発
+code --install-extension ms-vscode-remote.remote-containers
+@REM リモート開発
+code --install-extension ms-vscode.remote-explorer
+code --install-extension ms-vscode.remote-server
+
+@REM 結果をファイルに出力
+) >> Result.txt
+
+
+
