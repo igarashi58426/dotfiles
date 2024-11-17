c348fedf8e7c310567056bfa0b929d3300ffc8e0
igarashi58426 <igarasi0719@gmail.com>
2024-11-17 15:00:45 +0900

    linux_エイリアスの追加

--- Linux/set_alias.sh
+++ Linux/set_alias.sh
@@ -3,11 +3,12 @@
 # カレントディレクトリの移動
 cd `dirname $0`
 
-echo "alias ll='ls -l'" >> ~/.bashrc
+echo "alias ll='ls -alFh'" >> ~/.bashrc
 echo "alias la='ls -A'" >> ~/.bashrc
 echo "alias l='ls -CF'" >> ~/.bashrc
 echo "alias gr='grep'" >> ~/.bashrc
-echo "alias start='sudo systemctl start'" >> ~/.bashrc
-echo "alias status='sudo systemctl status'" >> ~/.bashrc
+echo "alias ..='cd ..'" >> ~/.bashrc
+echo "alias gr='grep'" >> ~/.bashrc
+echo "alias ii='explorer.exe'" >> ~/.bashrc
 
 source ~/.bashrc
