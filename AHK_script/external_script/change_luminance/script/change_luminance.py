17246ad6098a0d3103df536d35051b0fa15bfdd4
igarashi58426 <igarasi0719@gmail.com>
2025-12-23 15:16:42 +0900

    AHK_モニター輝度変更で定常エラー無視機能を追加

--- AHK_script/external_script/change_luminance/script/change_luminance.py
+++ AHK_script/external_script/change_luminance/script/change_luminance.py
@@ -7,16 +7,21 @@ else:
     add_luminance = 0
 
 # モニターオブジェクトを取得
-for monitor in monitorcontrol.get_monitors():
-    with monitor:
-        # 現在の輝度を取得
-        now_luminance = monitor.get_luminance()
-        # 第2引数を加算して変更後の輝度数値を算出
-        future_luminance = now_luminance + add_luminance
-        # 0~100の範囲に収める
-        if 100 < future_luminance:
-            future_luminance = 100
-        if future_luminance < 0:
-            future_luminance = 0
-        # モニターの輝度をセットする
-        monitor.set_luminance(future_luminance)
+try:
+    for monitor in monitorcontrol.get_monitors():
+        with monitor:
+            # 現在の輝度を取得
+            now_luminance = monitor.get_luminance()
+            # 第2引数を加算して変更後の輝度数値を算出
+            future_luminance = now_luminance + add_luminance
+            # 0~100の範囲に収める
+            if 100 < future_luminance:
+                future_luminance = 100
+            if future_luminance < 0:
+                future_luminance = 0
+            # モニターの輝度をセットする
+            monitor.set_luminance(future_luminance)
+except monitorcontrol.vcp.vcp_abc.VCPError as e:
+    print(f"定常エラー|{e}")
+except Exception as e:
+    raise e
